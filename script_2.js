3e3c1442fb1432d009172973f9fb72d9714a61a1
jesseyproctor <jss.proctor@gmail.com>
2020-11-18 22:45:36 -0500

    js-variables

--- script_2.js
+++ script_2.js
@@ -1,5 +1,19 @@
-const getPosts = () => {
-    return fetch(`https://exchangeratesapi.io/`)
-        .then(res => res.json())
-        .then(posts => console.log(posts))
-}
+
+fetch(`https://api.exchangeratesapi.io/latest`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+
+const baseCurrency = document.getElementById('select-base-currency')
+const targetCurrency = document.getElementById('select-target-currency')
+const convert = document.getElementById('convert-button')
+const amountToBeConverted = document.getElementById('currency-amount')
+
+convert.addEventListener('click', function() {
+    console.log("convert!")
+})
+
+amountToBeConverted.addEventListener('input', function() {
+    console.log("do math")
+})
